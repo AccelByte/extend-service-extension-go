b8ad836c3b153d3e80646c74d30704d041e26017
Dhanar Adi Dewandaru <dhanar@accelbyte.net>
2025-03-21 09:28:56 +0700

    feat(gateway): handle sigterm properly

--- main.go
+++ main.go
@@ -18,6 +18,7 @@ import (
 	"os/signal"
 	"path/filepath"
 	"strings"
+	"syscall"
 	"time"
 
 	"github.com/go-openapi/loads"
@@ -235,8 +236,10 @@ func main() {
 	logrus.Infof("gRPC server started")
 	logrus.Infof("app server started on base path: " + common.BasePath)
 
-	ctx, _ = signal.NotifyContext(ctx, os.Interrupt)
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
+	defer stop()
 	<-ctx.Done()
+	logrus.Infof("signal received")
 }
 
 func newGRPCGatewayHTTPServer(
